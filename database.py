4d6ca07560800c9f02835031cf7e9a3c00d04430
auksasful <akurat2012@gmail.com>
2026-01-04 23:02:27 +0100

    [FIX] Database pinging

--- database.py
+++ database.py
@@ -13,19 +13,23 @@ DB_HOST = os.getenv("DB_HOST")
 DB_NAME = os.getenv("DB_NAME")
 
 if not DB_PASS:
-    raise ValueError("No DB_PASSWORD found in .env file")
+    if os.getenv("AZURE_CONNECTION_STRING"): 
+        pass 
+    else:
+        raise ValueError("No DB_PASSWORD found in .env file")
 
 # Encode password to handle special characters (e.g. @, #, /)
-encoded_password = urllib.parse.quote_plus(DB_PASS)
+encoded_password = urllib.parse.quote_plus(DB_PASS) if DB_PASS else ""
 
 # CONNECTION STRING
 SQLALCHEMY_DATABASE_URL = f"postgresql://{DB_USER}:{encoded_password}@{DB_HOST}/{DB_NAME}"
 
-# CREATE ENGINE (With SSL)
-# Azure requires SSL connection by default. We add "sslmode": "require".
+# CREATE ENGINE (With SSL & Connection Keep-Alive)
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL,
-    connect_args={"sslmode": "require"} 
+    connect_args={"sslmode": "require"}, 
+    pool_pre_ping=True,  
+    pool_recycle=1800    
 )
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
